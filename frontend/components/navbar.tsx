4e8cd5729b8be392eb2a395e6e0868a3c3da7ac5
Bernardo Ferreira <bernardo.ferreira@knowledge-values.com>
2026-04-09 16:45:02 +0100

    lol

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -68,7 +68,7 @@ export function Navbar() {
         <div className="flex items-center justify-end gap-3">
           <div className="hidden items-center gap-3 xl:flex">
             <a href={`tel:${restaurantInfo.phonePrimary}`} className="button-secondary">
-              Reservar por telefone Bernarod
+              Reservar por telefone
             </a>
             <Link href="/reservations" className="button-primary">
               Reservar mesa
